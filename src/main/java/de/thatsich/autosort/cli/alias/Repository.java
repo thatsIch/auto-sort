bee4578231691fa0932a8ec62c9d4757e1408419
thatsIch <thatsIch@mail.de>
2018-02-23 11:58:42 +0100

    Rename generics to single letters

--- src/main/java/de/thatsich/autosort/cli/alias/Repository.java
+++ src/main/java/de/thatsich/autosort/cli/alias/Repository.java
@@ -4,12 +4,12 @@ import java.io.UnsupportedEncodingException;
 import java.util.Map;
 import java.util.Optional;
 
-public interface Repository<Key, Value> {
-	void persist(Key key, Value value) throws UnsupportedEncodingException;
+public interface Repository<K, V> {
+	void persist(K key, V value) throws UnsupportedEncodingException;
 
-	Optional<Value> find(Key key);
+	Optional<V> find(K key);
 
-	Optional<Value> remove(Key alias) throws UnsupportedEncodingException;
+	Optional<V> remove(K alias) throws UnsupportedEncodingException;
 
-	Map<Key, Value> unmodifiable();
+	Map<K, V> unmodifiable();
 }
