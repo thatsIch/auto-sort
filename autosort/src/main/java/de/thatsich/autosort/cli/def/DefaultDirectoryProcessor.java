a23d9d89e97bceeba263b5523878fe403627c475
thatsIch <thatsIch@mail.de>
2018-02-27 14:48:00 +0100

    Refactored DefaultDirectoryProcessor with compose logic

--- autosort/src/main/java/de/thatsich/autosort/cli/def/DefaultDirectoryProcessor.java
+++ autosort/src/main/java/de/thatsich/autosort/cli/def/DefaultDirectoryProcessor.java
@@ -12,6 +12,7 @@ import org.apache.logging.log4j.Logger;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Optional;
 
 public class DefaultDirectoryProcessor implements Processor<Path> {
 	private static final String SHORT_COMMAND = null;
@@ -47,41 +48,61 @@ public class DefaultDirectoryProcessor implements Processor<Path> {
 	@Override
 	public Path processCommandLine(CommandLine cl) {
 		if (cl.hasOption(LONG_COMMAND)) {
-			final String[] filterArgs = cl.getOptionValues(LONG_COMMAND);
+			final String[] args = cl.getOptionValues(LONG_COMMAND);
 
-			// we only get 'default' as command thus we want to print the default directory
-			if (filterArgs == null) {
-				final String defaultDirectory = persistence.retrieve();
-				if (defaultDirectory.isEmpty()) {
-					LOGGER.warn("Default directory has yet to be set. Defaulting to current working directory. Use the option 'default <directory>'.");
-				}
-				final Path defaultDirectoryPath = Paths.get(defaultDirectory).toAbsolutePath();
-
-				LOGGER.info("Default directory: " + defaultDirectoryPath.toString());
-
-				return defaultDirectoryPath;
-			// guard against invalid inputs
-			} else if (filterArgs.length > MAX_ARGS) {
-				final Options options = new Options();
-				options.addOption(this.constructOption());
-				this.helpPrinter.printOptions(options);
-
-				throw new IllegalArgumentException("At least '" + MAX_ARGS + "' arguments required but given '" + filterArgs.length + "' with '" + Arrays.toString(filterArgs) + "'.");
-			}
-			// we have exactly the required directory
-			// we want to set the directory
-			else {
-				final String maybeDirectory = filterArgs[0];
-				final Path defaultDirectoryPath = Paths.get(maybeDirectory).toAbsolutePath();
-				final String stringified = defaultDirectoryPath.toString();
-				persistence.persist(stringified);
-
-				LOGGER.info("Stored '" + stringified + "' as default directory.");
-
-				return defaultDirectoryPath;
-			}
+			return this.setDefaultDirectory(args)
+					.or(() -> this.tooManyArgs(args))
+					.or(() -> this.readDefaultDirectory(args))
+					.orElseThrow(() -> new IllegalStateException("Found an unhandled case with args '" + Arrays.toString(args)));
 		}
 
 		return null;
+	}
+
+	private Optional<Path> setDefaultDirectory(String[] args) {
+		// we only get 'default' as command thus we want to print the default directory
+		if (args == null) {
+			final String defaultDirectory = persistence.retrieve();
+			if (defaultDirectory.isEmpty()) {
+				LOGGER.warn("Default directory has yet to be set. Defaulting to current working directory. Use the option 'default <directory>'.");
+			}
+			final Path defaultDirectoryPath = Paths.get(defaultDirectory).toAbsolutePath();
+
+			LOGGER.info("Default directory: " + defaultDirectoryPath.toString());
+
+			return Optional.of(defaultDirectoryPath);
+		}
+
+		return Optional.empty();
+	}
+
+	private Optional<Path> tooManyArgs(String[] args) {
+		// guard against invalid inputs
+		if (args.length > MAX_ARGS) {
+			final Options options = new Options();
+			options.addOption(this.constructOption());
+			this.helpPrinter.printOptions(options);
+
+			throw new IllegalArgumentException("At least '" + MAX_ARGS + "' arguments required but given '" + args.length + "' with '" + Arrays.toString(args) + "'.");
+		}
+
+		return Optional.empty();
+	}
+
+	private Optional<Path> readDefaultDirectory(String[] args) {
+		// we have exactly the required directory
+		// we want to set the directory
+		if (args.length == 1) {
+			final String maybeDirectory = args[0];
+			final Path defaultDirectoryPath = Paths.get(maybeDirectory).toAbsolutePath();
+			final String stringified = defaultDirectoryPath.toString();
+			persistence.persist(stringified);
+
+			LOGGER.info("Stored '" + stringified + "' as default directory.");
+
+			return Optional.of(defaultDirectoryPath);
+		}
+
+		return Optional.empty();
 	}
 }
